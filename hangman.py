3580602d8ef4b69c9fbcdfbcd6033e379e9f03b4
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-05-12 08:32:54 -0500

    Added functions to main loop

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 class Hangman:
     def __init__(self):
@@ -6,22 +7,46 @@ class Hangman:
         self.state = "" #A list that tracks the player's correct/incorrect guesses, ex. "f__k"
         #print(self.wordList)
         self.lives = 10 #An int that represents the number of remaining attempts
+        self.wordToGuess = "hello"
 
     def playGame(self):
         "Begin the main game loop for a single game. Can be called again to start another game"
-        wordToGuess = random.choice(self.wordList).rstrip() #Pick a random word
-        self.setState(wordToGuess)
+        #self.wordToGuess = random.choice(self.wordList).rstrip() #Pick a random word
+        self.setState()
         while True:
             self.sendStateToPlayer()
+            userGuess = self.receiveInputFromPlayer()
+            self.updateStateAccordingToGuess(userGuess)
             #print(wordToGuess)
-            break #TODO: Remove later when done
+            #break #TODO: Remove later when done
 
     def sendStateToPlayer(self):
-        print()
+        "Displays the state on the player's end along with a reminder of how many lives are left"
+        print(''.join(self.state))
+        print(f"You have {self.lives} left.")
+
+    def receiveInputFromPlayer(self):
+        while True:
+            guess = input("Input your guess: ")
+            if re.fullmatch("[a-zA-Z]", guess) == None: #Use regex to verify that guess is a single letter
+                print("Guess must be a single letter.")
+                continue
+            else:
+                break
+        return guess
     
-    def setState(self, word):
-        "Sets self.state based on the word"
-        self.state = len(word) * "_"
+    def updateStateAccordingToGuess(self, userGuess):
+        "Replaces the blank spaces if the user's guess is correct"
+        for i in range(len(self.wordToGuess)):
+            if self.wordToGuess[i] == userGuess:
+                self.state[i] = userGuess + " "
+        print(self.state)
+    
+    def setState(self):
+        "Sets initial value of self.state based on the word"
+        self.state = []
+        for i in range(len(self.wordToGuess)):
+            self.state.append("- ")
         #print(self.state)
 
 def loadWordsFromFile(filename):
@@ -37,6 +62,3 @@ def loadWordsFromFile(filename):
 
 h = Hangman()
 h.playGame()
-string = "hi"
-string[1] = "a"
-print(string)
